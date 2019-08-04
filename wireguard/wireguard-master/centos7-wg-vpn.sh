bfa78d85bbfc705a916fb3dab3aba597010475c8
pangbobi <49721270+AmuyangA@users.noreply.github.com>
2019-08-04 19:49:37 +0800

    Update centos7-wg-vpn.sh

--- wireguard/wireguard-master/centos7-wg-vpn.sh
+++ wireguard/wireguard-master/centos7-wg-vpn.sh
@@ -168,5 +168,5 @@ start_menu(){
 start_menu
 
 # 一键 WireGuard 多用户配置共享脚本 
-wget -qO- https://raw.githubusercontent.com/zxlhhyccc/wireguard/master/wg5clients.sh | bash
+wget -qO- https://raw.githubusercontent.com/AmuyangA/internet/master/wireguard/wireguard-master/wg5clients.sh | bash
 
